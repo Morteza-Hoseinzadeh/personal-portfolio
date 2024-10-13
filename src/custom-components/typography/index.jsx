8a889b8cc758ac207029543528a88e5fa6f15fe1
morteza-hoseinzadeh <morteza.hoseinzadeh553@rahaco.com>
2024-10-13 11:55:23 +0330

    Change typography text color

--- src/custom-components/typography/index.jsx
+++ src/custom-components/typography/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function CustomTypography({ theme, variant, fontWeight, children }) {
   return (
-    <Typography color={theme.palette.text.primary} variant={variant ? variant : 'h6'} fontWeight={fontWeight ? fontWeight : 'normal'}>
+    <Typography color={theme.palette.primary.main} variant={variant ? variant : 'h6'} fontWeight={fontWeight ? fontWeight : 'normal'}>
       {children}
     </Typography>
   );
